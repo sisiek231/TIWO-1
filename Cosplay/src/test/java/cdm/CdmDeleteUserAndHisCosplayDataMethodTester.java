2025f77c4d755c1d359d0710baf69e324e3c0ee2
Michał Jereczek <michaljereczek@gmail.com>
2015-11-22 17:38:28 +0100

    dodanie klas testujacych (do zaimplementowania)

---
+++ Cosplay/src/test/java/cdm/CdmDeleteUserAndHisCosplayDataMethodTester.java
@@ -0,0 +1,19 @@
+package cdm;
+
+import org.junit.Test;
+
+/**
+ * Created by jereczem on 22.11.15.
+ */
+public class CdmDeleteUserAndHisCosplayDataMethodTester extends CdmTester{
+    /*
+     * Każda klasa testująca powinna rozwijać klasę abstrakcyjną CdmTester,
+     * dzięku czemu przed testowaniem każdej metody CAŁA baza danych jest usuwana, dlatego możemy przewidzieć dokładnie
+     * jak powinna ona wyglądać po przeprowadzeniu operacji, które testujemy.
+     */
+
+    @Test
+    public void correctData() throws Exception{
+
+    }
+}
