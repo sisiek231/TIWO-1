5f8f99a9d066d51a9dd3c566cfaab3942e718dc4
Darek <dark21pl@hotmail.com>
2015-11-24 22:35:58 +0100

    Test addCosplay
    test correctData

--- Cosplay/src/test/java/cda/AddCosplayMethodTester.java
+++ Cosplay/src/test/java/cda/AddCosplayMethodTester.java
@@ -1,6 +1,12 @@
 package cda;
 
+import cosplay.CosplayEntity;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * Created by jereczem on 22.11.15.
@@ -14,6 +20,37 @@ public class AddCosplayMethodTester extends Tester {
 
     @Test
     public void correctData() throws Exception{
+        //addCosplay(Timestamp date, Boolean isFavourite, String characterName, String franchiseName, String userNick)
+        //Używając testowanej metody dodaję nowy cosplay
+        String nick = "TestUser";
+        int age = 69;
+        CosplayDatabaseAPI.addUser(nick, age);
 
+        String name = "TestFranchise";
+        String genre = "Mahou Shoujo";
+        CosplayDatabaseAPI.addFranchise(name, genre);
+
+        Date d = new Date();
+
+        Timestamp date = new Timestamp(d.getTime());
+        Boolean isFavourite = true;
+        String characterName = "TestCharacter";
+        String franchiseName = "TestFranchise";
+        String userNick = "TestUser";
+
+        CosplayDatabaseAPI.addCosplay(date, isFavourite, characterName, franchiseName, userNick);
+
+        //Pobieram dane o cosplayach (klasa CosplayDatabaseData nie wymaga testowania, używamy jej do porównywania wyników)
+        CosplayDatabaseData cDD = new CosplayDatabaseData();
+        ArrayList<CosplayEntity> cosplay = cDD.getCosplayList();
+
+        // Wiem, że baza danych była pusta na początku, więc po dodaniu powinien być tylko jeden użytkownik
+        // Sprawdzam, czy ten użytkownik zgadza się z tym, którego przed chwilą dodałem
+        Assert.assertEquals(cosplay.size(), 1);
+        Assert.assertEquals(date, cosplay.get(0).getDate());
+        Assert.assertEquals(isFavourite, cosplay.get(0).isFavourite());
+        Assert.assertEquals(characterName, cosplay.get(0).getCharacterName());
+        Assert.assertEquals(franchiseName, cosplay.get(0).getFranchiseByFranchiseId().getName());
+        Assert.assertEquals(userNick, cosplay.get(0).getUsersByUsersId().getNick());
     }
 }
