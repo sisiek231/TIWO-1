bcd51efd240ee1ac970bb91432f533c52bdfacd4
jkl94 <jax0503@gmail.com>
2015-11-25 21:31:59 +0100

    testy do DeleteUser i GetUserData

--- Cosplay/src/test/java/cda/DeleteUserAndHisCosplayDataMethodTester.java
+++ Cosplay/src/test/java/cda/DeleteUserAndHisCosplayDataMethodTester.java
@@ -1,19 +1,60 @@
 package cda;
 
+import cosplay.CosplayEntity;
+import cosplay.UsersEntity;
 import org.junit.Test;
 
-/**
- * Created by jereczem on 22.11.15.
- */
 public class DeleteUserAndHisCosplayDataMethodTester extends Tester {
     /*
-     * Każda klasa testująca powinna rozwijać klasę abstrakcyjną Tester,
-     * dzięku czemu przed testowaniem każdej metody CAŁA baza danych jest usuwana, dlatego możemy przewidzieć dokładnie
-     * jak powinna ona wyglądać po przeprowadzeniu operacji, które testujemy.
+    deleteUserAndHisCosplayData(String nick) throws CantFindTheUserException, StringLongerThan45Exception, EmptyStringException
      */
 
     @Test
     public void correctData() throws Exception{
+
+        Date da= new Date();
+        Timestamp data = new Timestamp(da.getTime());
+        Boolean isFavourite = true;
+        String nick ="testuser";
+        //dodanie urzydkownika - nie wymaga testu
+        CosplayDatabaseAPI.addUser("testuser", 6 );
+        CosplayDatabaseAPI.addFranchise("testf", "testgen");
+        CosplayDatabaseAPI.addCosplay(data, isFavourite, "testchara", "testf", "testuser");
+
+        //usuniecie urzydkowinka przy urzyciu testowanej metody
+        CosplayDatabaseAPI.deleteUserAndHisCosplayData(nick);
+
+        //sprawdzenie czy funkcja wykonała zadanie
+        CosplayDatabaseData cdd = new CosplayDatabaseData();
+        ArrayList<CosplayEntity> cosplay = cdd.getCosplayList();
+        ArrayList<UsersEntity> users = cdd.getUsersList();
+
+        // baza i lista uzytkownikow były puste przed dodaniem uzydkownika wiec powinna byc taka i po uzyciu testowanej funkcji
+        Assert.assertEquals(users.isEmpty(), true);
+        Assert.assertEquals(cosplay.isEmpty(), true);
+
+    }
+
+    @Test(expected = CosplayDatabaseAPI.CantFindTheUserException.class) //wyrzucony wyjatek?
+    public void wrongDataCantFindTheUserException() throws Exception {
+
+    //pusta baza wiec nie bedzie tego uzytkownika
+        String nick ="hakunamatata";
+        CosplayDatabaseAPI.deleteUserAndHisCosplayData(nick);
+    }
+
+    @Test(expected = CosplayDatabaseAPI.EmptyStringException.class) //wyrzucony wyjątek?
+    public void wrongDataEmptyStringException() throws Exception{
+
+        String nick ="";
+        CosplayDatabaseAPI.deleteUserAndHisCosplayData(nick);
+    }
+
+    @Test(expected = CosplayDatabaseAPI.StringLongerThan45Exception.class) // wyrzucony wyjątek?
+    public void wrongDataStringLongerThan45Exception() throws Exception{
+
+        String nick ="testusereetestusereetestusereetestusereetestusereetestuseree";
+        CosplayDatabaseAPI.deleteUserAndHisCosplayData(nick);
 
     }
 }
